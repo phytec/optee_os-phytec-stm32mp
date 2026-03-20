gradhocJcalvo/optee-graddeck

commit 60621b6b6a1ddfc16edf0d620727d6e45f1aa4f6

stm32mp1: use generic PM_HINT and associated macro
Update stm32mp1 low power function to handle the generic
PM_HINT_.*_STATE and macros PM_HINT_IS_STATE() PM_HINT_PLATFORM_STATE()
for STM32MP13 and STM32MP15 with STPMIC1.

Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Change-Id: If1f765386dd0370fffbb6e6581721cd402280fea
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/358953
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>

core/drivers/clk/clk-stm32mp13.c
@@ -3348,7 +3348,7 @@ static TEE_Result stm32_clock_pm(enum pm_op op, unsigned int pm_hint,
 	static int standby_prepared;
 
 	if (op == PM_OP_SUSPEND) {
-		if (pm_hint == STM32_PM_CSTOP_ALLOW_STANDBY_DDR_SR) {
+		if (PM_HINT_IS_STATE(pm_hint, CONTEXT)) {
 			stm32_clock_suspend();
 			standby_prepared = 1;
 		} else {

core/arch/arm/plat-stm32mp1/pm/context.c
@@ -522,6 +522,35 @@ static void gate_pm_context_clocks(bool enable)
 	}
 }
 
+static uint32_t get_pm_hint(unsigned int soc_mode)
+{
+	uint32_t pm_hint = 0U;
+
+	assert(soc_mode < STM32_PM_MAX_SOC_MODE);
+
+	switch (soc_mode) {
+	case STM32_PM_CSLEEP_RUN:
+	case STM32_PM_CSTOP_ALLOW_STOP:
+	case STM32_PM_CSTOP_ALLOW_LP_STOP:
+	case STM32_PM_CSTOP_ALLOW_LPLV_STOP:
+	case STM32_PM_CSTOP_ALLOW_LPLV_STOP2:
+		pm_hint = PM_HINT_CLOCK_STATE;
+		break;
+
+	default:
+	case STM32_PM_CSTOP_ALLOW_STANDBY_DDR_SR:
+	case STM32_PM_CSTOP_ALLOW_STANDBY_DDR_OFF:
+	case STM32_PM_SHUTDOWN:
+		pm_hint = PM_HINT_CONTEXT_STATE;
+		break;
+	};
+
+	pm_hint |= ((soc_mode << PM_HINT_PLATFORM_STATE_SHIFT) &
+		    PM_HINT_PLATFORM_STATE_MASK);
+
+	return pm_hint;
+}
+
 /*
  * Context (TEE RAM content + peripherals) must be restored
  * only if system may reach STANDBY state.
@@ -532,7 +561,7 @@ TEE_Result stm32mp_pm_save_context(unsigned int soc_mode)
 
 	save_time();
 
-	res = pm_change_state(PM_OP_SUSPEND, soc_mode);
+	res = pm_change_state(PM_OP_SUSPEND, get_pm_hint(soc_mode));
 	if (res)
 		return res;
 
@@ -565,14 +594,17 @@ void stm32mp_pm_restore_context(unsigned int soc_mode)
 		stm32mp1_clk_restore_context_for_stop();
 #endif
 
-	if (pm_change_state(PM_OP_RESUME, 0))
+	if (pm_change_state(PM_OP_RESUME, get_pm_hint(soc_mode)))
 		panic();
 
 	restore_time();
 }
 
-void stm32mp_pm_shutdown_context(void)
+void stm32mp_pm_shutdown_context(unsigned int soc_mode)
 {
+	if (pm_change_state(PM_OP_SUSPEND, get_pm_hint(soc_mode)))
+		panic();
+
 	gate_pm_context_clocks(true);
 	load_earlyboot_pm_mailbox();
 	enable_pm_mailbox(0);

core/arch/arm/plat-stm32mp1/pm/context.h
@@ -83,7 +83,7 @@ void stm32mp_cpu_reset_state(void);
 
 TEE_Result stm32mp_pm_save_context(unsigned int soc_mode);
 void stm32mp_pm_restore_context(unsigned int soc_mode);
-void stm32mp_pm_shutdown_context(void);
+void stm32mp_pm_shutdown_context(unsigned int soc_mode);
 void stm32mp_pm_wipe_context(void);
 
 #ifndef CFG_STM32MP1_OPTEE_IN_SYSRAM

core/arch/arm/plat-stm32mp1/pm/low_power.c
@@ -363,6 +363,7 @@ void __noreturn stm32_enter_cstop_shutdown(uint32_t mode)
 				    TARGET_CPU1_GIC_MASK);
 	}
 #endif
+	stm32mp_pm_shutdown_context(mode);
 
 	switch (mode) {
 	case STM32_PM_SHUTDOWN:
@@ -374,7 +375,6 @@ void __noreturn stm32_enter_cstop_shutdown(uint32_t mode)
 		}
 		break;
 	case STM32_PM_CSTOP_ALLOW_STANDBY_DDR_OFF:
-		stm32mp_pm_shutdown_context();
 		stm32_enter_cstop(mode);
 		dsb();
 		isb();

core/arch/arm/plat-stm32mp1/pm/psci.c
@@ -399,9 +399,6 @@ void __noreturn psci_system_off(void)
 
 	DMSG("core %u", get_core_pos());
 
-	if (pm_change_state(PM_OP_SUSPEND, soc_mode))
-		panic();
-
 	stm32_enter_cstop_shutdown(soc_mode);
 }
 

core/drivers/stm32_exti.c
@@ -488,8 +488,7 @@ stm32_exti_pm(enum pm_op op, unsigned int pm_hint,
 {
 	struct stm32_exti_pdata *exti = NULL;
 
-	if (!IS_ENABLED(CFG_STM32MP13) && !IS_ENABLED(CFG_STM32MP15) &&
-	    !PM_HINT_IS_STATE(pm_hint, CONTEXT))
+	if (!PM_HINT_IS_STATE(pm_hint, CONTEXT))
 		return TEE_SUCCESS;
 
 	SLIST_FOREACH(exti, &stm32_exti_list, link) {

core/drivers/stm32_gpio.c
@@ -1294,8 +1294,7 @@ stm32_gpio_sec_config_pm(enum pm_op op, unsigned int pm_hint,
 {
 	TEE_Result ret = TEE_ERROR_GENERIC;
 
-	if (!IS_ENABLED(CFG_STM32MP13) && !IS_ENABLED(CFG_STM32MP15) &&
-	    pm_hint != PM_HINT_CONTEXT_STATE)
+	if (!PM_HINT_IS_STATE(pm_hint, CONTEXT))
 		return TEE_SUCCESS;
 
 	if (op == PM_OP_RESUME)

phytec/optee_os-phytec-stm32mp

commit 388ca83eac9b687f47c55d40ab01692981b36e42

drivers: clk: stm32mp13: simplify stm32_clock_pm
Remove static variable standby_prepared in stm32_clock_pm() as
standby exit information is provided by pm_hint CONTEXT flags.

Fixes: 60621b6b6a1d ("stm32mp1: use generic PM_HINT and associated macro")
Signed-off-by: Patrick Delaunay <patrick.delaunay@foss.st.com>
Change-Id: I150dfb382d859983a99e105d02e57a5e9fc2477d
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/524723
ACI: CIBUILD <MDG-smet-aci-builds@list.st.com>
Reviewed-by: Gabriel FERNANDEZ <gabriel.fernandez@foss.st.com>
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Thomas BOURGOIN <thomas.bourgoin@foss.st.com>

core/drivers/clk/clk-stm32mp13.c
@@ -3417,20 +3417,16 @@ static void __maybe_unused stm32_clock_resume(void)
 static TEE_Result stm32_clock_pm(enum pm_op op, unsigned int pm_hint,
 				 const struct pm_callback_handle *hdl __unused)
 {
-	static int standby_prepared;
-
 	if (op == PM_OP_SUSPEND) {
 		if (PM_HINT_IS_STATE(pm_hint, CONTEXT)) {
 			stm32_clock_suspend();
-			standby_prepared = 1;
 		} else {
 			stm32mp1_clk_save_context_for_stop();
-			standby_prepared = 0;
 		}
 		clear_rcc_reset_status();
 	} else {
 		/* back from standby */
-		if (standby_prepared)
+		if (PM_HINT_IS_STATE(pm_hint, CONTEXT))
 			stm32_clock_resume();
 		else
 			stm32mp1_clk_restore_context_for_stop();